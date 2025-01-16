8d642b13846d6814ffc41e4af163dd82b1c62f4a
unknown <daltonflores@gmail.com>
2025-01-16 12:08:15 -0500

    final

--- EjemplosDos/src/paquete01/Principal.java
+++ EjemplosDos/src/paquete01/Principal.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete01;
 
 import paquete02.DatoPersonal;
+import paquete02.DatoTrabajo;
 import paquete03.DatosUbicacion;
 import paquete04.DatoAcademico;
 import paquete05.DatoFinal;
@@ -16,30 +12,42 @@ import paquete05.DatoFinal;
  */
 public class Principal {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         String nombre;
         String ciudad;
         String apellidoRetornado;
+        String nombreEmpresa;
+        String direccionEmpresa;
         
         double[] misNotas;
         double promedio;
+        double mejorNota;
         
         nombre = DatoPersonal.obtenerNombre();
         apellidoRetornado = DatoPersonal.obtenerApellido();
         ciudad = DatosUbicacion.obtenerCiudad();
         misNotas = DatoAcademico.obtenerNotas(4);
         promedio = DatoFinal.obtenerPromedio(misNotas);
+        nombreEmpresa = DatoTrabajo.nombreEmpresa();
+        direccionEmpresa = DatoTrabajo.direccionEmpresa();
+        mejorNota = DatoFinal.obtenerMejorNota(misNotas);
         
         
         System.out.printf("Los datos ingresados son:\n"
                 + "Nombre: %s\n"
                 + "Apellido: %s\n"
                 + "Ciudad: %s\n"
-                + "Promedio: %.2f\n",
+                + "Empresa: %s\n"
+                + "Direccion: %s\n"
+                + "Promedio: %.2f\n"
+                + "Nota mas alta: %.2f\n",
                 nombre,
                 apellidoRetornado,
                 ciudad,
-                promedio);
+                nombreEmpresa,
+                direccionEmpresa,
+                promedio,
+                mejorNota);
     }
     
 }
